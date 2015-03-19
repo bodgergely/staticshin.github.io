09b58b0fe0c835c4978defe20e06a992b2e77007
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2015-03-20 00:16:58 +0530

    Reached a good compromise I think

--- staticshin_main.js
+++ staticshin_main.js
@@ -6,19 +6,20 @@
 	return Math.floor(Math.random() * (max - min + 1)) + min;
     };
     var ad = ads[random_ad(0,2)];
-    var ad_column = $(".col-8-12").append("<div id='ad_div'></div>");
-   
-    moreMenu.onclick = function(){
+   $("#more-menu").click(function(){
 	menuGrid.style.display ==='none' ?
 	    menuGrid.style.display =  '': menuGrid.style.display = "none";  
-    };
+    });
     document.onkeydown = function(event){
 	if(event.keyCode===27){
 	    menuGrid.style.display="none";
 	}
     };
-    
-    $("#ad_div").load("/ads/"+ad,function(){
-	console.log("ad is loaded");
-    });
+    if( !$('#ad_div').length){
+	var ad_column = $(".col-8-12").append("<div id='ad_div'></div>");	
+	$("#ad_div").load("/ads/"+ad);
+    }
+    if($("#ad_div").length){
+	$("#ad_div").load("/ads/"+ad);
+    }
 })();

22c1525a7e7484ee402e7eb0b12ee0ec685632f0
brickcap <akshatjiwan@gmail.com>
2017-03-08 08:48:03 +0530

    successfully hijacked and referenced the links

--- study/index.js
+++ study/index.js
@@ -1,4 +1,17 @@
+$("a").click(function(){
+    console.log("clicked");
+    var url = $(this).attr('href');
+    var id = url;
+    console.log(id);
+
+    change_color(id);
+});
+
+var change_color = function(id){
+    $(id).css("background-color","yellow");
+};
+
 var url = document.URL;
 var id = "#"+url.split("#")[1];
-console.log(id);
-$(id).css("background-color","yellow");
+
+change_color(id);
